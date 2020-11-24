e318731c7d6c3a70a8aa0b64e010a8e8dc94ae61
alexandre leroux <alexandre.leroux.photographies@gmail.com>
2020-11-24 15:51:17 +0100

    base en cours

---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="fr">
+   <head>
+
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>index</title>
+      <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+      <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      <link rel="stylesheet" href="css/style.css">
+     
+   </head>
+
+
+
+   <body>
+
+
+
+      <div class="wrapper">
+
+      
+         <header class="lg">
+            <nav>
+               <div class="menu-icon">
+                  <i class="fa fa-bars fa-2x"></i>
+               </div>
+               <div class="logo ml-5 my-2">
+                  <img id="img_logo" src="medias/logo.png" alt="">
+               </div>
+               <div class="menu">
+                  <ul>
+                     <li><a href="#">Home</a></li>
+                     <li><a href="#">Se connecter</a></li>
+                     <li><a href="#">S'inscrire</a></li>
+                     <li><a href="#">Livre d'or</a></li>                   
+                  </ul>
+               </div>
+            </nav>
+         </header>
+
+         <div class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end">
+                        <h1 class="text-uppercase text-white font-weight-bold">la tech au service de vos services</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                         <p class="text-white-75 text-light font-weight-light mb-5">Une expérience innovante, parfois même déroutante, au coeur des systèmes mêlant les services aux services !</p>
+                        <a class="btn btn-primary btn-xl js-scroll-trigger" href="#ancre">About us</a>
+                    </div>
+                </div>
+            </div>
+</div>
+
+<div id="ancre"></div>
+
+<div class="container-fluid w-75 my-5 h-100" id="container_equipe">
+
+      <div id="equipe" class="row d-flex  min-vh-100 m-4">
+
+         <h1 class=" w-100 text-center m-5">NOTRE EQUIPE</h1>
+
+            <div class="d-flex w-100 flex-wrap justify-content-around">
+
+                  <div class="coll-4">
+
+                     <img class="m-4" src="medias/portrait.jpg" alt="">
+                     <h2 class="text-center">Richard</h2>
+
+                  </div>
+                  <div class="coll-4">
+
+                     <img class="m-4" src="medias/portrait.jpg" alt="">
+                     <h2 class="text-center">Sébastien</h2>
+
+                  </div>
+                  <div class="coll-4">
+
+                     <img class="m-4" src="medias/portrait.jpg" alt="">
+                     <h2 class="text-center">Alex</h2>
+
+                  </div>
+            </div>
+
+      </div>
+</div>
+
+
+
+<div class="containeur p-5">
+<hr class="divider-2 w-50" />
+   <div class="row">
+      <div class="col">
+         <h1 class="text-uppercase m-5 text-center">nos réalisations</h1>
+      </div>
+   </div>
+</div>
+
+<div id="carouselExampleIndicators" class="carousel slide w-75 mx-auto" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="medias/carroussel-1.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="medias/carroussel-2.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="medias/carroussel-3.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+
+<footer id="fh5co-footer" class="fh5co-bg" role="contentinfo">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row row-pb-md">
+				<div class="col-md-4 fh5co-widget">
+					<h3>A Little About Stamina.</h3>
+					<p class="text-secondary">Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta adipisci architecto culpa amet.</p>
+					<p><a class="btn btn-primary" href="#">Become A Member</a></p>
+				</div>
+                
+				<div class="col-md-8 d-flex">
+        
+					<div class="col-md-4 col-sm-4 col-xs-6">
+                   
+						<ul class="fh5co-footer-links">
+							<li><a href="#">Cardio</a></li>
+							<li><a href="#">Body Building</a></li>
+							<li><a href="#">Yoga</a></li>
+							<li><a href="#">Boxing</a></li>
+							<li><a href="#">Running</a></li>
+						</ul>
+					</div>
+
+					<div class="col-md-4 col-sm-4 col-xs-6">
+						<ul class="fh5co-footer-links">
+							<li><a href="#">Boxing</a></li>
+							<li><a href="#">Martial Arts</a></li>
+							<li><a href="#">Karate</a></li>
+							<li><a href="#">Kungfu</a></li>
+							<li><a href="#">Basketball</a></li>
+						</ul>
+					</div>
+
+					<div class="col-md-4 col-sm-4 col-xs-6">
+						<ul class="fh5co-footer-links">
+							<li><a href="#">Badminton</a></li>
+							<li><a href="#">Body Building</a></li>
+							<li><a href="#">Teams</a></li>
+							<li><a href="#">Advertise</a></li>
+							<li><a href="#">API</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block text-secondary">&copy; 2020 | All Rights Reserved.</small> 
+						<small class="block text-secondary">Powered by  Alex</small>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+      <script type="text/javascript" src="js/script.js"></script>
+   </body>
+</html>
